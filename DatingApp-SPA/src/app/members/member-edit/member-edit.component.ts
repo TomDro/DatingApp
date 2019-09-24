77452c87151191ebf33a4e5d31bfeffde5c90ddd
Tomasz Drobiec <tomasz.drobiec@ebicom.pl>
2019-09-24 19:31:26 +0200

    End of section 11

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -14,6 +14,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class MemberEditComponent implements OnInit {
   user: User;
   @ViewChild('editForm', { static: true }) editForm: NgForm;
+  photoUrl: string;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
     if (this.editForm.dirty) {
@@ -26,6 +27,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => (this.photoUrl = photoUrl));
   }
 
   updateUser() {
@@ -38,5 +40,9 @@ export class MemberEditComponent implements OnInit {
         this.alertify.error(error);
       }
     );
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 }
