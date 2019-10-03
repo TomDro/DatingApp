b5e229366a6908fa8ca17c24237b5310a4f9f197
Tomasz Drobiec <tomasz.drobiec@ebicom.pl>
2019-10-03 18:51:08 +0200

    End of section 15

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -13,7 +13,7 @@ export class UserService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParams?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
     let params = new HttpParams();
@@ -30,6 +30,12 @@ export class UserService {
       params = params.append('orderBy', userParams.orderBy);
     }
 
+    if (likesParams === 'likers') {
+      params = params.append('likers', 'true');
+    }
+    if (likesParams === 'likees') {
+      params = params.append('likees', 'true');
+    }
     return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params }).pipe(
       map(response => {
         paginatedResult.result = response.body;
@@ -53,5 +59,9 @@ export class UserService {
   }
   deletePhoto(userId: number, id: number) {
     return this.http.delete(`${this.baseUrl}users/${userId}/photos/${id}`);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(`${this.baseUrl}users/${id}/like/${recipientId}`, {});
   }
 }
