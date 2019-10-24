8f47c68ad9a97ff591a886005ef3b6f7828237da
Tomasz Drobiec <tomasz.drobiec@ebicom.pl>
2019-10-24 11:10:59 +0200

    End of section 16

--- DatingApp.API/Migrations/20191007163416_MessageEntityAdded.Designer.cs
+++ DatingApp.API/Migrations/20191007163416_MessageEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191007163416_MessageEntityAdded")]
+    partial class MessageEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -40,7 +42,7 @@ namespace DatingApp.API.Migrations
 
                     b.Property<bool>("IsRead");
 
-                    b.Property<DateTime>("MessageSent");
+                    b.Property<DateTime>("MessageSend");
 
                     b.Property<bool>("RecipientDeleted");
 
